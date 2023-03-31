e75df5ac0b19bf9ff977f3bc4d2fae9332ae6bd5
victorquantalept <victor@quantalept.com>
2023-03-31 13:18:35 +0530

    Initial basic ui created

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+
+//import Login from "./components/Login.vue";
+//import SignUp from "./components/SignUp.vue";
+import Main from "./components/Main.vue";
+
+</script>
+<template>
+  <v-app>
+    <v-app-bar app top>
+      <v-img
+        class="mx-2"
+        src="..//assets/logo.png"
+        max-height="70"
+        max-width="240"
+        contain
+        left
+      ></v-img>
+    
+      <v-toolbar-title>Menu Section</v-toolbar-title>
+      <v-btn @click="loginDialog = true"
+      
+      color="primary"
+      >Login</v-btn>
+    </v-app-bar>
+
+    
+    <br /><br /><br />
+   <Main />
+  </v-app>
+   
+</template>
+
+
+<style>
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-container class="fill-height fluid align-center justify-center">
+    <v-card class="mx-auto px-6 py-9" width="500" height="500" elevation="12">
+      <v-toolbar color="blue">
+        <v-icon> mdi-account</v-icon>
+        Login
+      </v-toolbar>
+      <br />
+      <v-card-subtitle> Hi, Please unlock your local vault </v-card-subtitle>
+      <br /><br />
+      <v-card-text>
+        <v-row>
+          <v-text-field label="Master Password" color="blue"> </v-text-field>
+        </v-row>
+        <br />
+        <v-row>
+          <v-col>
+            <br /><br />
+            <v-btn
+              absolute
+              top
+              :style="{ left: '50%', transform: 'translateX(-50%)' }"
+              color="primary"
+              v-on:click="login()"
+              >unlock</v-btn>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      password: "",
+    };
+  },
+};
+</script>
+<style>
+</style>
