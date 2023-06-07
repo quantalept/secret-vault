c88f6c8fbfd3030769bbaef28c1a84f66cf5f9c1
victorquantalept <victor@quantalept.com>
2023-06-07 09:09:10 +0530

    secret vault ui standard code issue - completed

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import Login from "./components/Login.vue";
 import SignUp from "./components/SignUp.vue";
 import Main from "./components/Main.vue";
 import Menubar from "./components/Menubar.vue";
-
+import Test1 from "./components/Test1.vue";
 
 
 export default defineComponent({
@@ -13,6 +13,7 @@ export default defineComponent({
     Main,
     Login,    
     Menubar,
+    Test1,
     
   },
   // data() {
@@ -22,13 +23,14 @@ export default defineComponent({
 </script>
 <template>
   <v-app app>
-    <Menubar />
+    <Menubar/>
+    
     <v-main>
       <Main />
     </v-main>
     
   
-  <!-- <Main/> -->
+  <!-- <Test1/> -->
   </v-app>
 </template>
 <style scoped>
