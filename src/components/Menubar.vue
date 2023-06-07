ef2d0f9fb7d8a442ca905c76327a51e158401a4d
victorquantalept <victor@quantalept.com>
2023-06-07 09:09:10 +0530

    secret vault ui standard code issue - completed

--- src/components/Menubar.vue
+++ src/components/Menubar.vue
@@ -1,30 +1,29 @@
 <template>
-    <v-app-bar app dark height="35" color="#E5E4E2" bottom>
-      <v-menu offset-y>
-            <template v-slot:activator="{ props }">
-                <v-btn text v-bind="props" v-on="on">Vault</v-btn>
-            </template>
-            <v-list>
-              <v-list-item>
-                <v-list-item-title>Preferences</v-list-item-title>
-              </v-list-item>
-              <v-list-item>
-                <v-list-item-title>Exit</v-list-item-title>
-              </v-list-item>
-          </v-list>
-        </v-menu>
-    </v-app-bar>
-  </template>
-  
-  <script>
-  export default {
-    name: 'AppBar',
-  }
-  </script>
-  
-  <style scoped>
-  .v-app-bar {
-    border-bottom: 1px solid lightgray;
-  }
-  </style>
-  
+  <v-app-bar app dark height="35" color="#E5E4E2" bottom>
+    <v-menu offset-y>
+          <template v-slot:activator="{ props }">
+              <v-btn text v-bind="props" v-on="on">Vault</v-btn>
+          </template>
+          <v-list>
+            <v-list-item>
+              <v-list-item-title>Preferences</v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title>Exit</v-list-item-title>
+            </v-list-item>
+        </v-list>
+      </v-menu>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'AppBar',
+}
+</script>
+
+<style scoped>
+.v-app-bar {
+  border-bottom: 1px solid lightgray;
+}
+</style>
