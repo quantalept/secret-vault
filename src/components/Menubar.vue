2cfe6601ab0db7e067e5370ec2659ced5485afd0
victorquantalept <victor@quantalept.com>
2023-06-20 18:02:12 +0530

    secret vault ui menu creation completed

--- src/components/Menubar.vue
+++ src/components/Menubar.vue
@@ -1,29 +1,65 @@
 <template>
-  <v-app-bar app dark height="35" color="#E5E4E2" bottom>
-    <!-- <v-menu offset-y> -->
+  <div>
+    <v-app-bar app dark height="35" color="#E5E4E2" bottom>
+      <v-toolbar-items>
+        <v-menu offset-y open-on-hover>
           <template v-slot:activator="{ props }">
-              <v-btn text v-bind="props" v-on="on">Vault</v-btn>
+            <v-btn text v-bind="props">vault</v-btn>
           </template>
           <v-list>
-            <v-list-item>
-              <v-list-item-title>Preferences</v-list-item-title>
-            </v-list-item>
-            <v-list-item>
-              <v-list-item-title>Exit</v-list-item-title>
-            </v-list-item>
-        </v-list>
-      <!-- </v-menu> -->
-  </v-app-bar>
-</template>
+            <v-list-item v-for="(menu, index) in Filemenu" :key="index" @click="selectSection(menu)">
 
+              <v-list-item-title>{{ menu.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+        <v-menu offset-y open-on-hover>
+          <template v-slot:activator="{ props }">
+            <v-btn text v-bind="props">edit</v-btn>
+          </template>
+          <v-list>
+            <v-list-item v-for="(menu, index) in Editmenu" :key="index" @click="selectSection(menu)">
+
+              <v-list-item-title>{{ menu.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+        <v-menu offset-y open-on-hover>
+          <template v-slot:activator="{ props }">
+            <v-btn text v-bind="props">pref</v-btn>
+          </template>
+        </v-menu>
+        <v-menu offset-y open-on-hover>
+          <template v-slot:activator="{ props }">
+            <v-btn text v-bind="props">about</v-btn>
+          </template>
+        </v-menu>
+      </v-toolbar-items>
+    </v-app-bar>
+  </div>
+</template>
 <script>
 export default {
-  name: 'AppBar',
-}
-</script>
+  data() {
+    return {
 
-<style scoped>
-.v-app-bar {
-  border-bottom: 1px solid lightgray;
-}
-</style>
+      Filemenu: [
+        { title: 'New' },
+        { title: 'Open' },
+        { title: 'Save' },        
+        { title: 'Exit' },
+      ],
+      Editmenu: [
+        { title: 'Find' },
+        { title: 'Replace' },
+      ],
+    };
+  },
+  methods: {
+    selectSection(item1) {
+      this.selectedSection = item1;
+      console.log("*******   ", this.selectedSection);
+    }
+  },
+};
+</script>
