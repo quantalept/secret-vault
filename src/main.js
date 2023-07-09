60f9af61e3ed7d0264a4fd3dfeb25305c5f2e476
andy <andy@quantalept.com>
2023-07-09 15:21:00 +0530

    Initial changes for themes

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 import '@mdi/font/css/materialdesignicons.css'
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import theme from './styles/theme.js'
 
 // Vuetify
 import 'vuetify/styles'
@@ -16,6 +16,7 @@ const vuetify = createVuetify({
   icons: {
     defaultSet: 'mdi'
   },
+  theme
 })
 
 createApp(App).use(vuetify).mount('#app')
