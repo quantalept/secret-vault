99230f8fb2353d52cf95193d83158691ed4dd8d8
victorquantalept <victor@quantalept.com>
2023-07-12 17:59:08 +0530

    panel separation completed

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,7 +3,7 @@ export default {
         colors: {
             background: '#FFFFFF',
             surface: '#FFFFFF',
-            primary: '#eeecec',
+            primary: '#f5f5f5',
             'primary-darken-1': '#3700B3',
             secondary: '#03DAC6',
             'secondary-darken-1': '#018786',
@@ -15,7 +15,7 @@ export default {
             customColor3: '#FF3333',
             customColor4: '#C0C0C0',
             dimgray: '#696969',
-
+            customColor1:'',
 
         },
     }
