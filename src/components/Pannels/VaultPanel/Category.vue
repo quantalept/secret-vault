9525dd5580593f6cef2587927f3ee42144053f24
victorquantalept <victor@quantalept.com>
2023-07-12 17:55:59 +0530

    Merge pull request #3 from quantalept/minor-fixes-n-menubar
    Fixed minor errors and added menubar example

---
+++ src/components/Pannels/VaultPanel/Category.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-list bg-color="#f5f5f5">
+        <v-list-subheader>Categories</v-list-subheader>
+        <v-list-item class="pl-14" v-for="(category, j) in Categories" :key="j" :value="category" color="rgb(49, 49, 204)">
+            <v-list-item-title
+            
+            >{{ category.title }}</v-list-item-title>
+        </v-list-item>
+    </v-list>
+</template>
+<script>
+export default {
+
+    data() {
+        return {
+            Categories: [
+                { title: "All Categories" },
+                { title: "Site Credentials" },
+                { title: "Documents" },
+                { title: "Form Data" },
+                { title: "Token API" },
+
+            ],
+        };
+    }
+}
+</script>
+<style scoped>
+
+
+.v-list-subheader {
+    color: black;
+    font-size: 18px;
+    background: rgb(164, 166, 170);
+    font-weight: bold;
+    justify-content: center;
+}
+.categoryselected {
+  color: rgb(49, 49, 204);
+  font-weight: bold;
+  font-size: 18px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+
 const vuetify = createVuetify({
   components,
   directives,
