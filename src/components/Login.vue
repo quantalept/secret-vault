f05f38dd45137078a45f8adc55417b527e675810
laptop <antony.richard@quantalept.com>
2023-07-20 13:04:59 +0530

    login-signup page changed

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,79 +1,56 @@
 <template>
-  <v-container>
-    <!-- <v-dialog v-model="loginDialog" pesistent width="600"> -->
-    <v-card class="mx-auto px-6 py-9" width="600" elevation="5">
-      <v-toolbar color="blue" dark height="35" class="sm-4">
-        <v-icon>mdi-lock-open</v-icon>
-
-        <v-toolbar-title class="d-flex justify-center mt-1"
-          >App Initialization</v-toolbar-title
-        >
-        <v-spacer></v-spacer>
-        <v-btn icon>
-          <v-icon>mdi-close</v-icon>
-        </v-btn>
+  <v-dialog v-model="showDialog" persistent max-width="600px"  >
+    <v-card color="#E5E4E2">
+      <v-toolbar color="#C0C0C0"  height="32">
+        <v-icon>mdi-lock</v-icon>
+        <v-toolbar-title class=" text-center" style="font-family:Lucida">App Initialization</v-toolbar-title>
       </v-toolbar>
-      <v-div>
-        <v-card-subtitle class="text-h6 ma-3 ml-2 block">
-          Hi, Please unlock your local vault
-        </v-card-subtitle>
-
-        <v-card-text>
-          <v-row>
-            <v-col cols="4" left>
-              <div class="input-label">Master Password:</div>
-            </v-col>
-            <v-col cols="8">
-              <v-text-field
-                v-model="password"
-                label="Master Password"
-                color="primary"
-                class="textalign"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-
-          <!-- <v-card-actions class="d-flex justify-center" color="primary"></v-card-actions> -->
-          <v-row>
-            <v-btn
-              absolute
-              top
-              :style="{ left: '50%', transform: 'translateX(-50%)' }"
-              color="primary"
-              v-on:click="login()"
-              >Unlock</v-btn
-            >
-          </v-row>
-        </v-card-text>
-      </v-div>
+      <v-card-subtitle class="subtitle  text-center">Hi, Please unlock your vault </v-card-subtitle>
+      <v-card-text>
+        <v-row >
+          <v-col cols="4">
+            <v-label class="input-label ">Master password :</v-label>
+          </v-col>
+          <v-col cols="6" class="input-field" >
+            <v-text-field  variant="solo" class=" ml-4" density="compact"></v-text-field>
+          </v-col>
+        </v-row>
+      </v-card-text>
+      <v-card-actions class="justify-center">
+        <v-btn variant="tonal" color="#0000FF" @click="login" class="button">Unlock</v-btn>
+      </v-card-actions>
     </v-card>
-  </v-container>
-  <!-- </v-dialog> -->
+  </v-dialog>
 </template>
+
 <script>
 export default {
   data() {
     return {
-      password: "",
-    };
+      showDialog: true,
+      
+    }
   },
-};
-</script>
-<style>
-.v-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
 }
-.v-toolbar-title {
-  font-size: 19px;
+</script>
+
+<style scoped>
+.subtitle {
+  margin-top: 45px;
+  font-weight: bold;
+  font-size: 18px;
+  color:black;
+  
 }
 .input-label {
-  margin-left: 10px;
-  margin-top: 15px;
+  font-weight: bold;
+  margin-top: 40px;
+  margin-left: 45px;
 }
-.textalign {
-  margin-left: -30px;
+.input-field {
+  margin-top: 35px;
+}
+.button {
+  margin-bottom: 50px;
 }
 </style>
