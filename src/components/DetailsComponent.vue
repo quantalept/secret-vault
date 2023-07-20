a2bcdcc004e653b42846808a75c448de816c8877
victorquantalept <victor@quantalept.com>
2023-07-20 16:47:03 +0530

    show panel2 details by click on panel1

---
+++ src/components/DetailsComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <v-card v-if="selectedItem">
+        <v-card-title>{{ selectedItem.name }}</v-card-title>
+        <v-card-text>
+          {{ selectedItem.description }}
+        </v-card-text>
+      </v-card>
+      <v-alert v-else>No item selected</v-alert>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      selectedItem: Object
+    }
+  };
+  </script>
+  
