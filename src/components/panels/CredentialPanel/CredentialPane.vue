b8165b5f6fd5a828a15b6bb4d417639b175e941b
victorquantalept <victor@quantalept.com>
2023-07-24 18:47:17 +0530

    completed the click and show panels and alignment

--- src/components/panels/CredentialPanel/CredentialPane.vue
+++ src/components/panels/CredentialPanel/CredentialPane.vue
@@ -1,23 +1,36 @@
 <template>
   <v-card height="100%" color="primary">
-    <v-form class="px-3">
+    <v-form class="px-3" v-if="selectedCred">
       <v-card-text>
         <v-row>
           <v-label class="ml-16 mr-10">Title:</v-label>
           <v-col cols="12" sm="4">
-            <v-text-field append-inner-icon="mdi-pencil" v-model="credData.title" class="mt-4" variant="solo"
-              density="compact" />
+            <v-text-field
+              append-inner-icon="mdi-pencil"
+              v-model="credData.title"
+              class="mt-4"
+              variant="solo"
+              density="compact"
+            />
           </v-col>
           <v-spacer></v-spacer>
-          <v-btn class='mt-5 md-5' icon="mdi-plus" @click="addTextField()">
+          <v-btn class="mt-5 md-5" icon="mdi-plus" @click="addTextField()">
           </v-btn>
-          <v-btn class='mt-5 md-5 ml-2' icon>
+          <v-btn class="mt-5 md-5 ml-2" icon>
             <v-icon>mdi-heart</v-icon>
           </v-btn>
         </v-row>
         <v-divider />
-        <div v-for="(field, index) in credData.fields" :key="index" class="creds-view">
-          <secret-row :label="field.label" :value="field.value" :valueType="field.valueType" />
+        <div
+          v-for="(field, index) in credData.fields"
+          :key="index"
+          class="creds-view"
+        >
+          <secret-row
+            :label="field.label"
+            :value="field.value"
+            :valueType="field.valueType"
+          />
         </div>
       </v-card-text>
       <v-card-actions>
@@ -28,52 +41,47 @@
     </v-form>
   </v-card>
 </template>
-
 <script>
-import SecretRow from './SecretRow.vue'
+import SecretRow from "./SecretRow.vue";
 
 export default {
+  props: {
+    selectedCred: Object,
+  },
   components: {
-    SecretRow
+    SecretRow,
   },
   setup() {
     const credData = {
-      "title": "Netflix",
-      "fields": [
+      title: "Netflix",
+      fields: [
         {
           label: "User Name",
           value: "testuser",
-          valueType: "text"
-
+          valueType: "text",
         },
         {
           label: "Password",
           value: "password",
-          valueType: "password"
-
+          valueType: "password",
         },
         {
           label: "PIN",
           value: "600041",
-          valueType: "password"
-
-        }
-
+          valueType: "password",
+        },
       ],
-
-    }
+    };
 
     return {
       credData,
-      isFavorite: false
-    }
-  }
-
-}
+      isFavorite: false,
+    };
+  },
+};
 </script>
-<style scoped>
-.credential-pane {}
-
+<style scoped>.credential-pane {
+}
 .creds-view {
   margin-top: 20px;
 }
