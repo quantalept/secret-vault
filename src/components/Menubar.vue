921fdfeeff2bb69aacfa193f9e816155cccaaed6
victorquantalept <victor@quantalept.com>
2023-08-09 18:49:54 +0530

    secret manager adding help menu and its contents in menu bar - completed

--- src/components/Menubar.vue
+++ src/components/Menubar.vue
@@ -31,9 +31,33 @@
         </v-menu>
         <v-menu offset-y open-on-hover>
           <template v-slot:activator="{ props }">
-            <v-btn text v-bind="props">about</v-btn>
+            <v-btn text v-bind="props">help</v-btn>
           </template>
+          <v-list>
+            <v-list-item
+              v-for="(menu, index) in Helpmenu"
+              :key="index"
+              @click="openHelp(menu)"
+            >
+              <v-list-item-title>{{ menu.title }}</v-list-item-title>
+              
+            </v-list-item>
+          </v-list>
         </v-menu>
+        <v-dialog v-model="dialog" max-width="500">
+          <v-card   >
+            <v-toolbar height="30">            
+            <v-toolbar-title bg-color="blue">   {{ selectedFile.title }}                         
+            </v-toolbar-title>
+            <v-card-actions>  
+            <v-btn> <v-icon icon="mdi-close" @click="dialog=false"></v-icon></v-btn>    
+          </v-card-actions>     
+          </v-toolbar>
+            <v-card-text>
+              {{selectedFile.content}}
+            </v-card-text>            
+          </v-card>
+        </v-dialog>
       </v-toolbar-items>
     </v-app-bar>
   </div>
@@ -42,7 +66,8 @@
 export default {
   data() {
     return {
-
+      selectedFile: null,
+      dialog: false,
       Filemenu: [
         { title: 'New' },
         { title: 'Open' },
@@ -53,12 +78,21 @@ export default {
         { title: 'Find' },
         { title: 'Replace' },
       ],
+      Helpmenu: [
+        { title: "Getting Started",content:"Content of Getting Started" },
+        { title: "Documentation", content:"Content of Documentation" },
+        { title: "Check for Updates", content:"Content of Check for Updates" },
+        { title: "About", content:"Secret Manager is a secure and convenient storage system for API keys, passwords, certificates, and other sensitive data." },
+      ],
     };
   },
   methods: {
-    selectSection(item1) {
-      this.selectedSection = item1;
-      console.log("*******   ", this.selectedSection);
+    openHelp(helpitem) {
+      this.selectedFile = helpitem;
+      this.dialog = true;
+    },
+    closeVault() {
+      this.dialog = false;
     }
   },
 };
